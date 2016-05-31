d28bb7b79c92ac1b383fd965d06e271fe1ed101b
Todd Cardoso <crystal@Crystals-MacBook-Air.local>
2016-05-30 17:26:48 -0700

    Add bid_controller tests

--- spec/controllers/bids_controller_spec.rb
+++ spec/controllers/bids_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe BidsController, type: :controller do
 
+  let(:auction) { FactoryGirl.create(:auction) }
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe "#new" do
+    context "without a signed in user" do
+      it "redirects to sign up page" do
+        get :new, auction_id: auction.id
+        expect(response).to redirect_to(new_user_path)
+      end
+    end
+    context "with signed in user" do
+
+      before { sign_in user }
+      it "renders the new template" do
+        get :new, auction_id: auction.id
+        expect(response).to render_template(:new)
+      end
+
+      it "assigns a new bid instance variable" do
+        get :new, auction_id: auction.id
+        expect(assigns(:bid)).to be_a_new(Bid)
+      end
+    end
+  end
 end
